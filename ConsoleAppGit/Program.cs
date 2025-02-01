dff75d5b6e39f7a09146ac49cee5ae5f21a01e24
raffaele.filomena <raffaele.filomena@PC2222BIS>
2025-02-01 09:34:04 +0100

    cambio main

--- ConsoleAppGit/Program.cs
+++ ConsoleAppGit/Program.cs
@@ -5,8 +5,9 @@
         static void Main(string[] args)
         {
             Dado d=new Dado();
-            int valore = d.lanciaDado();
-            Console.WriteLine(valore);
+            int valore1 = d.lanciaDado();
+            int valore2 = d.lanciaDado();
+            Console.WriteLine($"{valore1} {valore2}");
         }
     }
 }
