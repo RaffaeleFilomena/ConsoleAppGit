8e195c286614af8583a91d6869afa4c9ec56b9b8
raffaele.filomena <raffaele.filomena@PC2222BIS>
2025-02-01 09:24:37 +0100

    aggiunta classe Dado e modifica del main

---
+++ ConsoleAppGit/Dado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGit
+{
+    internal class Dado
+    {
+        public int lanciaDado()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 7);
+        }
+    }
+}
